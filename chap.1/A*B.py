de4810394eb89302f37461336a03f0d32399276a
KHson <zzzzax93@gmail.com>
2017-08-15 18:17:12 +0900

    장별로 묶기

---
+++ chap.1/A*B.py
@@ -0,0 +1,19 @@
+#Q: 두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+
+#IN: 첫째 줄에 A와 B가 주어진다. (0 < A,B < 10)
+
+#OUT: 첫째 줄에 A*B를 출력한다.
+
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+def multi(x,y):
+    temp = x * y
+    return temp
+
+if a>0:
+    if b<11:
+        result = multi(a,b)
+        print(result)
---
+++ chap.1/A-B.py
@@ -0,0 +1,10 @@
+#3.
+#직전 문제와 동일
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+if a > 0:
+    if b < 11:
+        result = a - b
+        print(result)
