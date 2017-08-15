86c13a28b1851ac50d85cb0d6165d65165e9b1cf
KHson <zzzzax93@gmail.com>
2017-08-15 17:14:15 +0900

    3. pass

--- A-B.py
+++ A-B.py
@@ -1,11 +1,10 @@
 #3.
 #직전 문제와 동일
-a = int(input('A를 입력하세요(조건: 0 < A) : '))
-b = int(input('B를 입력하세요(조건: B < 10) : '))
+a, b = input().split()
+a = int(a)
+b = int(b)
 
-if a < 1:
-    print('조건 불충족, A가 0이하.')
-if b > 9:
-    print('조건 불충족, B가 10이상.' )
-else:
-    print('A - B = ', a-b)
+if a > 0:
+    if b < 11:
+        result = a - b
+        print(result)
