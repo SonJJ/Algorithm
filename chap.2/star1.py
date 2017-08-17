6426f3283b1ad4156836b7d5862d143531f4696e
KHson <zzzzax93@gmail.com>
2017-08-17 13:42:04 +0900

    입력조건문 수정

--- chap.2/star1.py
+++ chap.2/star1.py
@@ -7,8 +7,9 @@
 n = int(input())
 
 def star(x):
-    for i in range(0,n):
-        dot = "*"
-        temp = dot*(i+1)
-        print(temp)
+    if 1<=x<=100:
+        for i in range(0,n):
+            dot = "*"
+            temp = dot*(i+1)
+            print(temp)
 star(n)
