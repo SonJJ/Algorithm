b4dfa6ab56fa964f4168a5d9239e0ba4fb65d293
KHson <zzzzax93@gmail.com>
2017-08-17 14:02:14 +0900

    pass 별찍기 N to 1

---
+++ chap.2/star3.py
@@ -0,0 +1,15 @@
+#Q: 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+
+#IN: 첫째 줄에 N (1<=N<=100)이 주어진다.
+
+#OUT: 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
+
+n = int(input())
+
+def star(x):
+    if 1<=x<=100:
+        for i in range(0,n):
+            dot = "*"
+            temp = dot*(n-i)
+            print(temp)
+star(n)
